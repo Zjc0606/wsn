db864d4bd97d65b454b5cae6ec90e5a94b9ad9a3
Zjc0606 <1067299908@qq.com>
2017-10-23 08:54:26 +0800

    session共享

---
+++ src/main/java/com/zjc/ssm/condition/BaseCondition.java
@@ -0,0 +1,27 @@
+package com.zjc.ssm.condition;
+
+/**
+ * @author 安徽大学.赵继传
+ * @Description:
+ * @create 2017-10-22-13:39
+ */
+public class BaseCondition {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- src/main/java/com/zjc/ssm/controller/LoginController.java
+++ src/main/java/com/zjc/ssm/controller/LoginController.java
@@ -1,14 +1,15 @@
 package com.zjc.ssm.controller;
 
 import com.alibaba.fastjson.JSONObject;
+import com.zjc.ssm.condition.BaseCondition;
 import com.zjc.ssm.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 /**
  * Created by zjc on 2017/5/24.
@@ -21,21 +22,34 @@ public class LoginController {
 
     // 商品查询
     @RequestMapping("/test")
-    public ModelAndView queryItems(HttpServletRequest request) throws Exception {
+    @ResponseBody
+    public JSONObject queryItems(HttpServletRequest request, BaseCondition baseCondition) throws Exception {
         //测试forward后request是否可以共享
-        System.out.println("1111111");
-
-
-        return null;
+        HttpSession session = request.getSession();
+        String ss = (String) session.getAttribute("username");
+        JSONObject result = new JSONObject();
+        if (baseCondition.getUsername().equals(ss)) {
+            result.put("msg","session已存在");
+            result.put("sessionId", session.getId());
+            result.put("username", session.getAttribute("username"));
+            result.put("password", session.getAttribute("password"));
+        } else {
+            result.put("msg","session不存在");
+            session.setAttribute("username", baseCondition.getUsername());
+            session.setAttribute("password", baseCondition.getPassword());
+        }
+        return result;
     }
 
     //@RequestParam里边指定request传入参数名称和形参进行绑定。
-    @RequestMapping(value="/login")
+    @RequestMapping(value = "/login")
     @ResponseBody
-    public JSONObject Login(String username, String password)throws Exception {
-
+    public JSONObject Login(HttpServletRequest request) throws Exception {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+        String password = (String) session.getAttribute("password");
         //调用service根据用户名和密码判断登录信息
-        JSONObject result= loginService.login(username, password);
+        JSONObject result = loginService.login(username, password);
 
         return result;
     }
